a345c2b2da88cacde3052eb6c9fd0976106dfdec
haroon <haroon.seena2017@gmail.com>
2023-06-23 01:06:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+function findMatching(driver,string){
+    return driver.filter(drive => drive.toLowerCase() === string.toLowerCase());
+}
+function fuzzyMatch(driver, string){
+    return driver.filter(drive => drive.toLowerCase().startsWith(string.toLowerCase()));
+}
+
+function matchName(driver, string){
+    return driver.filter(drive.name.toLowerCase() === string.toLowerCase());
+}
